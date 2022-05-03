34a422defa58d7eb0c2f213fe394877a93b0bcfb
Egor Gulid <egor-gulid@yandex.ru>
2022-05-04 01:04:00 +0300

    start project 3DGlo

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import burger from './modules/burger'
+import sliderOne from './modules/slider'
+import sliderTwo from './modules/badScript'
+import oneModule from './modules/one'
+import twoModule from './modules/two'
+
+burger()
+sliderOne()
+sliderTwo()
+oneModule()
+twoModule()
